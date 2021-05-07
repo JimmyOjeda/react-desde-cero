ad500b76c16e80d2e85e896d613a2507899c680b
JimmyOjeda <ojedajimmy1@gmail.com>
2021-05-07 01:28:07 -0500

    Primer commit del curso React.js desde cero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import "./styles/styles.scss";
+import Curso from './Curso'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <Fragment>
+    <div className="main-banner img-container l-section" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img className="main-banner__img" alt="Banner" src="https://es.uadyvirtual.uady.mx/pluginfile.php/1/core_admin/logo/0x200/1620050216/Header_logoUADYV.png" />
+          <div className="main-banner__data s-center">
+            <p className="t2 s-mb-0">Mi super banner</p>
+            <p> Una chafa de bootstrap</p>
+            <a href="./" className="button">Botonazo</a>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
+
+    
+    <div className="ed-grid m-grid-4">
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+    </div>
+  </Fragment>
+);
 
 export default App;
